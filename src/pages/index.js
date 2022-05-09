f414ce0902d866250106a4b4ab9dd1039c32c193
eduarmreyes <eduar.mreyes@gmail.com>
2022-05-09 14:48:12 +0100

    update packages and add Image from next

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import Button from "@components/Button";
 
-import products from '@data/products';
+import products from "@data/products";
 
-import styles from '@styles/Page.module.scss'
+import styles from "@styles/Page.module.scss";
 
 export default function Home() {
   return (
@@ -27,7 +28,19 @@ export default function Home() {
                 <h2>Prepare for liftoff.</h2>
                 <p>Apparel that&apos;s out of this world!</p>
               </div>
-              <img className={styles.heroImage} src="/images/space-jelly-gear-banner.jpg" alt="" />
+              <Image
+                className={styles.heroImage}
+                src="/images/space-jelly-gear-banner.jpg"
+                alt="Two people showing off the shirt and hat apparel."
+                layout="responsive"
+                height={272}
+                width={800}
+              />
+              {/* <img
+                className={styles.heroImage}
+                src="/images/space-jelly-gear-banner.jpg"
+                alt=""
+              /> */}
             </a>
           </Link>
         </div>
@@ -35,32 +48,31 @@ export default function Home() {
         <h2 className={styles.heading}>Featured Gear</h2>
 
         <ul className={styles.products}>
-          {products.slice(0, 4).map(product => {
+          {products.slice(0, 4).map((product) => {
             return (
               <li key={product.id}>
                 <Link href="#">
                   <a>
                     <div className={styles.productImage}>
-                      <img width="500" height="500" src={product.image} alt="" />
+                      <Image
+                        width={500}
+                        height={500}
+                        src={product.image}
+                        alt={`${product.name} showing on a product card.`}
+                      />
                     </div>
-                    <h3 className={styles.productTitle}>
-                      { product.name }
-                    </h3>
-                    <p className={styles.productPrice}>
-                      ${ product.price }
-                    </p>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
                   </a>
                 </Link>
                 <p>
-                  <Button>
-                    Add to Cart
-                  </Button>
+                  <Button>Add to Cart</Button>
                 </p>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
 }
